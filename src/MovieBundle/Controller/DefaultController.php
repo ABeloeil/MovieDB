bcdb3b4374d46a1ad281cf23b190294061f26c9f
ABeloeil <arnaud.beloeil@gmail.com>
2015-05-29 22:05:32 +0200

    use annotation routing

--- src/MovieBundle/Controller/DefaultController.php
+++ src/MovieBundle/Controller/DefaultController.php
@@ -3,11 +3,21 @@
 namespace MovieBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ * Class DefaultController
+ * @package MovieBundle\Controller
+ * @Route("/", name="movie_bundle_default")
+ */
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * @Route("/", name="movie_bundle_default_index")
+     */
+    public function indexAction()
     {
-        return $this->render('MovieBundle:Default:index.html.twig', array('name' => $name));
+        return new JsonResponse('true');
     }
 }
