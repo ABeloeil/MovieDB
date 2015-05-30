c041d0206eb736a8b1a71a6a9ebb23e310c48b23
ABeloeil <arnaud.beloeil@gmail.com>
2015-05-30 08:03:46 +0200

    add movie to render

--- src/MovieBundle/Controller/MovieController.php
+++ src/MovieBundle/Controller/MovieController.php
@@ -4,6 +4,8 @@ namespace MovieBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class MovieController
@@ -14,10 +16,41 @@ class MovieController extends Controller {
 
     /**
      * @Route("/", name="movie_bundle_movie_index")
+     * @Method({"GET"})
      */
     public function indexAction()
     {
-
+        return $this->render(':movie:index.html.twig');
     }
 
+    /**
+     * @Route("/add", name="movie_bundle_movie_add")
+     * @Method({"GET", "POST"})
+     */
+    public function addAction(Request $request)
+    {
+        return $this->render(':movie:add.html.twig');
+    }
+
+    /**
+     * @Route("/{id}/edit", name="movie_bundle_movie_edit", requirements={"id"="\d+"})
+     * @Method({"GET","POST"})
+     */
+    public function editAction(Request $request, Movie $movie)
+    {
+        return $this->render(':movie:edit.html.twig',[
+            'movie' => $movie,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/delete", name="movie_bundle_movie_delete", requirements={"id"="\d+"})
+     * @Method({"GET","POST"})
+     */
+    public function deleteAction(Request $request, Movie $movie)
+    {
+        return $this->render(':movie:delete.html.twig',[
+            'movie' => $movie,
+        ]);
+    }
 }
